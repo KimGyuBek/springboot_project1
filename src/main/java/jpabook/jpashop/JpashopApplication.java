483aa5e8197021a6340ab05121c76172a25c30b7
rlarbqor00 <rlarbqor00@naver.com>
2022-07-21 23:17:42 +0900

    [add]add entities

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -9,8 +9,6 @@ public class JpashopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
 
-		Member member = new Member();
-		member.setUsername("1234");
 
 
 	}
